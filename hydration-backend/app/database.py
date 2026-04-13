fd810e58a19c06184c8b266074ca53da16467aef
Jasmine Gatewood <jass.gatewood@outlook.com>
2026-04-13 13:44:47 -0400

    removed old backend and docker files.

--- hydration-backend/app/database.py
+++
@@ -1,9 +0,0 @@
-
-from pymongo import MongoClient
-import os
-
-MONGO_URL = os.environ["MONGO_URL"]
-
-client = MongoClient(MONGO_URL, serverSelectionTimeoutMS=5000)
-db = client["hydration_db"]
-events_collection = db["events"]
--- hydration-backend/app/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class HydrationEvent(BaseModel):
-    type: str
-    amount_ml: float
-    timestamp: int
